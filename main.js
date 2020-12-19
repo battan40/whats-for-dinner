6eb7eda23c9bbd8f58a95c83cba4093281376c02
Angela Battillo <angela.battillo@gmail.com>
2020-12-19 16:59:46 -0700

    Interactive functionality of buttons and image

--- main.js
+++ main.js
@@ -5,28 +5,71 @@ var sideRadioButton = document.querySelector('#side');
 var mainDishRadioButton = document.querySelector('#main-dish');
 var dessertRadioButton = document.querySelector('#dessert')
 var entireMealRadioButton = document.querySelector('#entire-meal');
+
 var letsCookButton = document.querySelector('.lets-cook');
+var whatAreYouLookingFor = document.querySelector('.what-are-you-looking-for');
+var crockpotView = document.querySelector('.crockpot-view');
+var youShouldMakeView = document.querySelector('.you-should-make');
+
+// var radioButtonReadsAs = document.querySelector('input[name="dinner"]:checked').value
 
 
 //Arrays:
+mainDishes = ['Shakshuka','Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup', 'Ramen', 'Empanadas', 'Tofu Fried Rice', 'Crab Cakes'];
+sides = ['Miso Glazed Veggies', 'Garden Salad', 'Crispy Potatoes', 'Sweet Potato Fries', 'Jasmine Rice', 'Caeser Salad', 'Hush Puppies'];
+desserts = ['Tres leches','Baklava', 'Flan', 'Key Lime Pie', 'Pain Au Chocolat', 'Kheer', 'Three Chocolate Chip Cookies', 'Macarons', 'Masala Chai Spice Cake', 'Tart Tatin'];
+//possible empty array for possibly interpolated meals to be injected into
 
-var mealList = ;
+// class FullMenu {
+//   constructor(side, main, dessert, entireMeal) {
+//   this.sidesMealChoice = side,
+//   this.mainDishMealChoice = main,
+//   this.dessertMealChoice = dessert,
+//   this.entireMealChoice = entireMeal;
+// }
+//
+// }
 
-//Possible Global Variables?:
+//Global Variable:
+choice = '';
 
 //Event Listeners:
+letsCookButton.addEventListener('click', showRandomSelection);
+
 
 //functions:
 
-function makeRandomMealAppear() {
-  event.preventDefault() {
-    var randomMealAppears = mealList[getRandomIndex(mealList)];
-  }
-}
+function showRandomSelection() {
+  event.preventDefault();
+  youShouldMakeView.classList.remove('hidden')
+  crockpotView.classList.add('hidden')
+  var randomMainDish = mainDishes[getRandomIndex(mainDishes)];
+  var randomside = sides[getRandomIndex(sides)];
+  var randomdessert = desserts[getRandomIndex(desserts)];
 
+
+}
 function getRandomIndex(array){
   return Math.floor(Math.random() * array.length);
+}
 
+function choiceSide() {
+
+}
+function choiceMainDish() {
+
+}
+function choiceDessert() {
+
+}
+
+function bringMealTogether() {
+  event.preventDefault();
+    var randomMainDish = mainDishes[getRandomIndex(mainDishes)];
+    var randomside = sides[getRandomIndex(sides)];
+    var randomdessert = desserts[getRandomIndex(desserts)];
+
+  //currentMealChoice = new Entire Meal(randomMainDish, randomSide, randomDessert)
 }
 
 function clearMealSelection(){
@@ -39,4 +82,8 @@ function goHome(){
 
 function showEntireMeal(){
 
+}
+
+function addAsterisk(){
+  return
 }
