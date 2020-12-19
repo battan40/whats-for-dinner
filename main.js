a2b8658b5811cfea35732e48f85e53fc80fd34f3
Angela Battillo <angela.battillo@gmail.com>
2020-12-19 12:01:06 -0700

    Adjust button view with js skeleton

--- main.js
+++ main.js
@@ -1,2 +1,42 @@
-var mainPage = ;
-var 
+//Query Selectors:
+
+var addRecipeButton = document.querySelector('.button-add-recipe');
+var sideRadioButton = document.querySelector('#side');
+var mainDishRadioButton = document.querySelector('#main-dish');
+var dessertRadioButton = document.querySelector('#dessert')
+var entireMealRadioButton = document.querySelector('#entire-meal');
+var letsCookButton = document.querySelector('.lets-cook');
+
+
+//Arrays:
+
+var mealList = ;
+
+//Possible Global Variables?:
+
+//Event Listeners:
+
+//functions:
+
+function makeRandomMealAppear() {
+  event.preventDefault() {
+    var randomMealAppears = mealList[getRandomIndex(mealList)];
+  }
+}
+
+function getRandomIndex(array){
+  return Math.floor(Math.random() * array.length);
+
+}
+
+function clearMealSelection(){
+
+}
+
+function goHome(){
+
+}
+
+function showEntireMeal(){
+
+}
