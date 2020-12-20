c11364ecfd7642862cceb46f676a34e50365fefb
Angela Battillo <angela.battillo@gmail.com>
2020-12-19 20:16:36 -0700

    Clean comment areas out

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ var whatAreYouLookingFor = document.querySelector('.what-are-you-looking-for');
 var crockpotView = document.querySelector('.crockpot-view');
 var youShouldMakeView = document.querySelector('.you-should-make');
 var randomDish = document.querySelector('.random-meal-appears-here');
-// var radioButtonReadsAs =) document.querySelector('input[name="dinner"]:checked').value
+
 
 //Global Variable:
 var choice = '';
@@ -48,43 +48,13 @@ function choiceDessert() {
   return desserts[getRandomIndex(desserts)]+ '!';
 }
 
-function bringMealTogether() {
-  // event.preventDefault();
-  // youShouldMakeView.classList.remove('hidden')
-  // crockpotView.classList.add('hidden')
-  // randomDish.classList.remove('hidden')
-//this.choice =
-  //currentMealChoice = new Entire Meal(randomMainDish, randomSide, randomDessert)
-}
-
 function clearMealSelection(){
-
 }
 
 function goHome(){
-
-}
-
-function showEntireMeal(){
-
-}
-
-function addAsterisk(){
-  return
 }
 
 //Arrays:
 mainDishes = ['Shakshuka','Thai Yellow Curry', 'Bibimbap', 'Chicken Parmesean', 'Butternut Squash Soup', 'Ramen', 'Empanadas', 'Tofu Fried Rice', 'Crab Cakes'];
 sides = ['Miso Glazed Veggies', 'Garden Salad', 'Crispy Potatoes', 'Sweet Potato Fries', 'Jasmine Rice', 'Caeser Salad', 'Hush Puppies'];
 desserts = ['Tres leches','Baklava', 'Flan', 'Key Lime Pie', 'Pain Au Chocolat', 'Kheer', 'Three Chocolate Chip Cookies', 'Macarons', 'Masala Chai Spice Cake', 'Tart Tatin'];
-//possible empty array for possibly interpolated meals to be injected into
-
-// class FullMenu {
-//   constructor(side, main, dessert, entireMeal) {
-//   this.sidesMealChoice = side,
-//   this.mainDishMealChoice = main,
-//   this.dessertMealChoice = dessert,
-//   this.entireMealChoice = entireMeal;
-// }
-//
-// }
